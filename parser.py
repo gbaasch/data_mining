eb92e8c8595960a3a2684ce113b20fcc312a7802
Gregory <gregoryrohagan@gmail.com>
2019-02-21 11:37:44 -0800

    Added basic parser. TODO: needs comments

---
+++ parser.py
@@ -0,0 +1,27 @@
+
+inpath = "data/merchandise_values_annual_dataset.csv"
+outpath = "data/output.tsv"
+
+def main():
+	connections = []
+	with open(inpath, 'r') as infile:
+		lines = infile.readlines()[1:]
+
+		for line in lines:
+			line = line.split(',')
+			if line[7].startswith("\"Exports"):
+				newConn = [line[1], line[3]]
+				if newConn not in connections:
+					connections.append(newConn)
+
+	with open(outpath, 'w+') as outfile:
+		for i in range(4):
+			outfile.write("#this is an empty line\n")
+		for conn in connections:
+			for item in conn:
+				outfile.write(str(item) + "\t")
+			outfile.write("\n")
+
+
+if __name__ == '__main__':
+	main()
