30bb85c096d909005cad6dd37ec2c50bf04b2b58
Gregory <gregoryrohagan@gmail.com>
2019-02-21 13:34:33 -0800

    added scratch file for getting totals by category

---
+++ scratch_totals_by_category.py
@@ -0,0 +1,62 @@
+inpath = "C:/Workspace/data_mining/data/merchandise_values_annual_dataset.csv"
+
+categories = ['Total merchandise', 'Agricultural products', 'Manufactures',
+				'Fuels and mining products', 'Food', 'Clothing', 'Textiles',
+				'Office and telecom equipment', 'Chemicals',
+				'Machinery and transport equipment', 'Iron and steel',
+				'Automotive products', 'Fuels', 'Transport equipment',
+				'Telecommunications equipment',
+				'Electronic data processing and office equipment', 'Pharmaceuticals',
+				'Integrated circuits and electronic components', 'Other food products',
+				'Other semi-manufactures', 'Other chemicals', 'Other manufactures',
+				'Raw materials', 'Scientific and controlling instruments',
+				'Other machinery', 'Ores and other minerals',
+				'Personal and household goods', 'Non-ferrous metals',
+				'Miscellaneous manufactures', 'Other transport equipment', 'Fish']
+
+
+def main():
+	category_dict_import = {}
+	category_dict_export = {}
+	importTotal = 0
+	exportTotal = 0
+	with open(inpath, 'r') as infile:
+		lines = infile.readlines()[1:]
+
+		for line in lines:
+			line = line.split('","')
+			import_type_val = category_dict_import.get(line[5])
+
+
+			if line[7].startswith("Exports"):
+				#if line[0].startswith("\"AF"):
+				exportTotal += float(line[-4])
+				export_type_val = category_dict_export.get(line[5])
+				export_type_val = 0 if export_type_val is None else export_type_val + float(line[-4])
+				category_dict_export[line[5]] = export_type_val
+			if line[7].startswith("Imports"):
+				#if line[2].startswith("\"AF"):
+				importTotal += float(line[-4])
+				import_type_val = category_dict_export.get(line[5])
+				import_type_val = 0 if import_type_val is None else import_type_val + float(line[-4])
+				category_dict_import[line[5]] = import_type_val
+
+	# for inCon in imports:
+	# 	if inCon not in exports:
+	# 		print(inCon)
+	#
+	# print(importTotal)
+	# print(exportTotal)
+	for key in categories:
+		export = 0 if category_dict_export.get(key) is None else category_dict_export.get(key)
+		importt = 0 if category_dict_import.get(key) is None else category_dict_import.get(key)
+		if export:
+			print(key, (export - importt) / export, export / exportTotal)
+		else:
+			print(key + " does not exist")
+
+	print(category_dict_export)
+	print(category_dict_import)
+
+if __name__ == '__main__':
+	main()
