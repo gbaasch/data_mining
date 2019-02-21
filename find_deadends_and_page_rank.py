859a51a823500c4af60798474a959b5936e12c01
gbaasch <gaby.baasch@gmail.com>
2019-02-21 12:02:02 -0800

    print dead ends for imports and exports

--- find_deadends_and_page_rank.py
+++ find_deadends_and_page_rank.py
@@ -1,7 +1,13 @@
 from link_analysis.question_1 import get_deadends
 
-filename = 'output.tsv'
+filename_ie = 'output-data/imports_exports.tsv'
+filename_e = 'output-data/exports.tsv'
+filename_i = 'output-data/imports.tsv'
 
 if __name__ == '__main__':
-    deadends, _, _ = get_deadends(filename, encoding="ISO-8859-1")
-    print(deadends)
+    deadends_ie, _, _ = get_deadends(filename_ie, encoding="ISO-8859-1")
+    deadends_e, _, _ = get_deadends(filename_e, encoding="ISO-8859-1")
+    deadends_i, _, _ = get_deadends(filename_i, encoding="ISO-8859-1")
+    print("imports and exports: ", deadends_ie)
+    print("\n\nexports: ", deadends_e)
+    print("\n\nimports: ", deadends_i)
